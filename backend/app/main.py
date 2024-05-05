64d884e54f7f7cdbe4e63297814b09d1311c5b32
Mally-cj <601882280@qq.com>
2024-05-05 12:03:01 +0800

    feat: 支持后端热重载

--- backend/app/main.py
+++ backend/app/main.py
@@ -35,4 +35,4 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8088)
+    uvicorn.run("main:app", host="127.0.0.1", port=8088, reload=True)
