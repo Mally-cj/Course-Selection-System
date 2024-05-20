e691daf726b60a1a58fb63af9aa8212a200a5336
martinASchen <694482753@qq.com>
2024-05-20 13:31:04 +0200

    教师信息

--- frontend/src/components/Teachers/teacherDetail.tsx
+++ frontend/src/components/Teachers/teacherDetail.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Box, Button, Container, Divider, Grid, GridItem, Heading, Text } from "@chakra-ui/react";
+import { Box, Button, Container, Divider, Grid, GridItem, Heading, Text ,Avatar } from "@chakra-ui/react";
 import { Block } from "@tanstack/react-router";
 import { Flex } from "antd";
+
+import Icon from '../../assets/images/defaultTeaIcon.jpg'
 
 function TeacherDetails({ teachers, onClose }) {
     const headerStyle = {
@@ -19,6 +21,10 @@ function TeacherDetails({ teachers, onClose }) {
                     <Heading size="lg">教师详细信息</Heading>
                 </Box>
                 <Divider borderColor="blue.500" />
+
+                <Flex align="center" justify="space-around">
+                <Avatar marginLeft="-50" src={Icon} size="2xl" ></Avatar>
+
                 <Grid templateColumns="repeat(2, 1fr)" gap={6} mt={4}>
                     <GridItem>
                         <Text fontSize="lg" fontWeight="bold"><strong>姓名：</strong>{teachers.name}</Text>
@@ -26,57 +32,51 @@ function TeacherDetails({ teachers, onClose }) {
                     <GridItem>
                         <Text fontSize="lg" fontWeight="bold"><strong>邮箱：</strong>{teachers.email}</Text>
                     </GridItem>
+                   
                     <GridItem>
-                        <Text fontSize="lg" fontWeight="bold"><strong>学号：</strong>{teachers.id}</Text>
-                    </GridItem>
-                    <GridItem>
-                        <Text fontSize="lg" fontWeight="bold"><strong>职称：</strong>{teachers.major}</Text>
+                        <Text fontSize="lg" fontWeight="bold"><strong>职称：</strong>{teachers.title}</Text>
                     </GridItem>
                     <GridItem>
                         <Text fontSize="lg" fontWeight="bold"><strong>大学：</strong>{teachers.college}</Text>
                     </GridItem>
+                    
                 </Grid>
+                </Flex>
+
                 <Divider my={4} borderColor="blue.500" />
                 <Box style={headerStyle}>
                     <Heading size="md">更多信息</Heading>
                 </Box>
                 <Grid templateColumns="repeat(2, 1fr)" gap={6} mt={4}>
                     <GridItem>
-                        <Text fontSize="lg"><strong>年龄：</strong>21</Text>
+                        <Text fontSize="lg"><strong>电话：</strong>029-8266-3266</Text>
                     </GridItem>
                     <GridItem>
-                        <Text fontSize="lg"><strong>年级：</strong>大三</Text>
+                        <Text fontSize="lg"><strong>个人主页：</strong>http://ygong.gr.xjtu.edu.cn</Text>
                     </GridItem>
                     <GridItem>
-                        <Text fontSize="lg"><strong>GPA：</strong>3.8</Text>
+                        <Text fontSize="lg"><strong>通讯地址</strong>陕西省西安市雁塔区</Text>
                     </GridItem>
                     <GridItem>
-                        <Text fontSize="lg"><strong>导师：</strong>张教授</Text>
+                        <Text fontSize="lg"><strong>邮政编码：</strong>710054</Text>
                     </GridItem>
-                    <GridItem>
-                        <Text fontSize="lg"><strong>联系方式：</strong>123-456-7890</Text>
-                    </GridItem>
-                    <GridItem>
-                        <Text fontSize="lg"><strong>地址：</strong>北京市朝阳区</Text>
-                    </GridItem>
+                    
                 </Grid>
                 <Divider my={4} borderColor="blue.500" />
                 <Box style={headerStyle}>
-                    <Heading size="md">选修课程</Heading>
+                    <Heading size="md">教育经历</Heading>
                 </Box>
                 <Grid templateColumns="repeat(2, 1fr)" gap={6} mt={4}>
                     <GridItem>
-                        <Text fontSize="lg"><strong>课程1：</strong>自然语言处理</Text>
+                        <Text fontSize="lg">1983.04 -- 1987.03：日本东京大学，电气电子工学学士</Text>
                     </GridItem>
                     <GridItem>
-                        <Text fontSize="lg"><strong>课程2：</strong>计算机视觉</Text>
+                        <Text fontSize="lg">1987.04 -- 1989.03：日本东京大学，电气电子工学硕士</Text>
                     </GridItem>
                     <GridItem>
-                        <Text fontSize="lg"><strong>课程3：</strong>机器学习</Text>
+                        <Text fontSize="lg">1989.04 -- 1992.03：日本东京大学，电气电子工学博士</Text>
                     </GridItem>
-                    <GridItem>
-                        <Text fontSize="lg"><strong>课程4：</strong>深度学习</Text>
-                    </GridItem>
+                    
                 </Grid>
                 <Flex justify="center" align="center">
                     <Button mt={4} m="a"  colorScheme="teal" onClick={onClose}>返回</Button>
